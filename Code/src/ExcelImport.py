8621fc8dde1ae45663106abda945c89b5ff06bb1
Orlando Signer <orlando.signer@students.unibe.ch>
2014-03-05 11:20:21 +0100

    - Open excel file - build xlrd to read excel files

--- Code/src/ExcelImport.py
+++ Code/src/ExcelImport.py
@@ -3,16 +3,23 @@
 @author: orlando.signer@students.unibe.ch
 '''
 
+import xlrd
+
 class ExcelImport(object):
     '''
     classdocs
     '''
     
     def __init__(self, ):
-        print 'init'
         '''
         Constructor
         '''
         
     def importFile(self, filename):
+        workbook = xlrd.open_workbook(filename)
+        sheet = workbook.sheet_by_index(0)
+        for row in range(sheet.nrows):
+            print row
+            
+        workbook.release_resources()
         return None
