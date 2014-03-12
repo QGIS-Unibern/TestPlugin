7299c9abb8fbd1133fb2819ea3db0c4a696aa48c
Orlando Signer <orlando.signer@students.unibe.ch>
2014-03-12 11:44:45 +0100

    excel auslesen und liste mit variablen abfüllen

--- Code/ExcelImport/src/ExcelImport.py
+++ Code/ExcelImport/src/ExcelImport.py
@@ -18,8 +18,20 @@ class ExcelImport(object):
     def importFile(self, filename):
         workbook = xlrd.open_workbook(filename)
         sheet = workbook.sheet_by_index(0)
-        for row in range(sheet.nrows):
-            print sheet.row(row)
-            
+        guiElements = []
+        for i in range(sheet.nrows):
+            if i == 0: 
+                continue
+            row = sheet.row(i)
+            guiElements.append(GuiElement(row[1], row[4]))
+
+        
         workbook.release_resources()
         return None
+    
+class GuiElement(object):
+    def __init__(self, varId, name):
+        if varId is None or name is None:
+            raise ValueError("varId (%varId) or name (%name) is none!", varId, name)
+        self.varId = varId
+        self.name = name
