d3d32eb75f4336d990e7418f3c4cd62db86bf973
Orlando Signer <orlando.signer@students.unibe.ch>
2014-03-16 20:53:04 +0100

    replace name in ui-files

--- Code/ExcelImport/src/ExcelImport.py
+++ Code/ExcelImport/src/ExcelImport.py
@@ -12,32 +12,52 @@ class ExcelImport(object):
     classdocs
     '''
     
-    
-    
     def __init__(self, ):
         '''
         Constructor
         '''
         
+    def createPluginGui(self, excelPath, xmlPath, outputPath):
+        excelElements = self.importExcel(excelPath)
+        tree = ElementTree.parse(xmlPath)
+        widgets = self.getXmlWidgets(tree)
+        
+        print widgets
+        
+        for widget in widgets:
+            id = int(widget.get('name')[-3:])
+            name = excelElements[id]
+            if name is None:
+                print 'id: {0} is none'.format(id)
+            else:
+                textProp = widget.find("./property[@name='text']")
+                if textProp is not None:
+                    textProp.find("./string").text = name
+        
+        tree.write(outputPath)
+        
+        
+        
     def importExcel(self, filename):
         workbook = xlrd.open_workbook(filename)
         sheet = workbook.sheet_by_index(0)
-        guiElements = []
+        guiElements = {}
         for i in range(sheet.nrows):
             if i == 0: 
                 continue # first line contains titles
             row = sheet.row(i)
-            guiElements.append(GuiElement(row[1], row[4]))
+            varId = round(row[1].value)
+            if guiElements.has_key(varId):
+                raise ValueError("multiple rows with id (%id)", varId)
+            guiElements[varId] = row[4].value
             
         workbook.release_resources()
         return guiElements
     
-    def importXML(self, filename):
-        with open(filename, 'r') as content_file:
-            content = content_file.read()
-        tree = ElementTree.XML(content)
+    def getXmlWidgets(self, tree):
         widgets = []
         self.addChildrenWidgets(tree.find("widget"), widgets)
+        return widgets
             
     def addChildrenWidgets(self, widget, children):
         for child in widget.findall("./"):
@@ -45,22 +65,6 @@ class ExcelImport(object):
                 name = child.get('name')
                 regex = re.compile('.*_\d{3}$', flags=re.M)
                 if regex.match(name):
-                    children.append(widget)
+                    children.append(child)
                     
             self.addChildrenWidgets(child, children)
-
-class GuiElement(object):
-    def __init__(self, varId, name):
-        if varId is None or name is None:
-            raise ValueError("varId (%varId) or name (%name) is none!", varId, name)
-        self.varId = varId
-        self.name = name
-        
-    def __str__(self, *args, **kwargs):
-        return str(self.__dict__)
-        
-    def __cmp__(self, other):
-        return self.varId == other.varId and self.name == other.name
-    
-    def __hash__(self, *args, **kwargs):
-        return hash(self.varId, self.name)
--- Code/ExcelImport/test/ExcelImportTest.py
+++ Code/ExcelImport/test/ExcelImportTest.py
@@ -5,12 +5,16 @@ Created on Mar 3, 2014
 '''
 import unittest
 import ExcelImport
-from ExcelImport import GuiElement
+from xml.etree import ElementTree
 
 CONST_EXCEl_FILENAME = "resources/20140301_Bern_Excel-Input-Tabelle_Attributnamen.xls"
 CONST_XML_FILENAME = "../../../Gui/GUIersterVersuchWoche3.ui"
 
 class ExcelImportTest(unittest.TestCase):
+    
+    def testCreatePlugin(self):
+        importer = ExcelImport.ExcelImport()
+        importer.createPluginGui(CONST_EXCEl_FILENAME, CONST_XML_FILENAME, 'output.ui')
 
     
     def testImportExcel(self):
@@ -18,11 +22,17 @@ class ExcelImportTest(unittest.TestCase):
         result = importer.importExcel(CONST_EXCEl_FILENAME)
         self.assertNotEqual(None, result, 'result is none')
         self.assertEquals(42, len(result))
-        self.assertEquals(GuiElement(100, 'Nr.'), result[0])
+        self.assertEquals('Nr.', result[100])
         
     def testImportXml(self):
+        with open(CONST_XML_FILENAME, 'r') as content_file:
+            content = content_file.read()
+        tree = ElementTree.XML(content)
         importer = ExcelImport.ExcelImport()
-        importer.importXML(CONST_XML_FILENAME)
+        result = importer.getXmlWidgets(tree)
+        
+        self.assertNotEqual(None, result)
+        self.assertEqual(86, len(result))
         
 
 
