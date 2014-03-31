c3eda34538402444a50a27b6f25ed9c97f0c5ea0
Stephan Matter <stephan.matter@students.unibe.ch>
2014-03-31 16:28:32 +0200

    First working version of the spatiaLiteCreator

--- Code/SpatiaLiteCreator/src/SpatiaLiteCreator.py
+++ Code/SpatiaLiteCreator/src/SpatiaLiteCreator.py
@@ -6,17 +6,28 @@ import MasterPluginGuiCreator as guiCreator
 from pyspatialite import dbapi2 as db
 
 def createSpatiaLiteDatabase(excelPath, projectName):
-    
+    '''
+    Main Method
+    Creates a Spatialite-database with given attribute Names as Excel sheet
+    The name of the file will be 'projectName.sqlite'
+    '''
     excelElements = guiCreator.importExcel(excelPath)
     attributeNames = extractAttributeNames(excelElements)
-    createTables(statNames, varNames)
+    createTables(attributeNames[0], attributeNames[1], projectName)
 
 def extractAttributeNames(excelElements):
     '''
     Gets attribute names and datatypes from the dictionary
     '''
-    for key, value in excelElements.iteritems():
-        value = 0
+    varNames = []
+    statNames = []
+    for key, ExcelRow in excelElements.iteritems():
+        if ExcelRow.isVariabel == 1:
+            varNames.append([ExcelRow.name, ' VARCHAR(50)'])
+        else:
+            statNames.append([ExcelRow.name, ' VARCHAR(50)'])
+    return [statNames, varNames]
+        
     
 def createTables(statNames, varNames, projectName):
     '''
@@ -24,19 +35,31 @@ def createTables(statNames, varNames, projectName):
     - one geometry-layer (polygon) which also stores constant data
     - one geometry-less layer which stores variable data
     '''
-    conn = db.connect(name+'.sqlite')
+    conn = db.connect(projectName+'.sqlite')
     cur = conn.cursor()
-    sql = 'SELECT InitSpatialMetadata()'
+    
+    sql = "SELECT InitSpatialMetadata()"
     cur.execute(sql)
-    sql = 'CREATE TABLE ' + projectName + 'const ('
-    sql += 'id INTEGER PRIMARY KEY AUTOINCREMENT,'
+    sql = "CREATE TABLE '" + projectName + "_const' ("
+    sql += "id INTEGER PRIMARY KEY AUTOINCREMENT,"
     for attribute in statNames:
-        sql += attribute + ' VARCHAR(50), '
+        sql += "'" + attribute[0] + "' " + attribute[1] + ","
+    sql = sql[:-1]
+    sql += ")"
+    print sql
     cur.execute(sql)
     
-    sql = 'CREATE TABLE ' + projectName + 'var ('
-    sql += 'id INTEGER PRIMARY KEY AUTOINCREMENT,'
-    sql += 'parent_id INTEGER REFERENCES ' + projectName + 'const,'
+    sql = "SELECT AddGeometryColumn('"+projectName+"_const', " 
+    sql += "'geometry', 4326, 'POLYGON', 'XY')" 
+    print sql
+    cur.execute(sql)
+    
+    sql = "CREATE TABLE '" + projectName + "_var' ("
+    sql += "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+    sql += "parent_id INTEGER REFERENCES '" + projectName + "_const',"
     for attribute in varNames:
-        sql += attribute + ' VARCHAR(50), '
+        sql += "'" + attribute[0] + "' " + attribute[1] + ","
+    sql = sql[:-1]
+    sql += ")"
+    print sql
     cur.execute(sql)
--- Code/SpatiaLiteCreator/test/SpatiaLiteCreatorTest.py
+++ Code/SpatiaLiteCreator/test/SpatiaLiteCreatorTest.py
@@ -2,7 +2,25 @@
 
 @author: stephan.matter@students.unibe.ch
 '''
-
 import unittest
 import sys, os
 sys.path.insert(0, os.path.dirname('../src/SpatiaLiteCreator.py'))
+import SpatiaLiteCreator as creator
+
+try: 
+    os.remove('test_simple.sqlite')
+    os.remove('test.sqlite')
+except OSError:
+    pass
+
+CONST_EXCEl_FILENAME = "resources/20140301_Bern_Excel-Input-Tabelle_Attributnamen.xls"
+CONST_EXCEl_FILENAME_SIMPLE = "resources/20140301_Bern_Excel-Input-Tabelle_Attributnamen_simple.xls"
+
+class SpatiaLiteCreatorTest(unittest.TestCase):
+    
+    def testSpatiaLiteCreatorSimple(self):
+        creator.createSpatiaLiteDatabase(CONST_EXCEl_FILENAME_SIMPLE, 'test_simple')
+        '''
+    def testSpatiaLiteCreator(self):
+        creator.createSpatiaLiteDatabase(CONST_EXCEl_FILENAME, 'test')
+        '''
