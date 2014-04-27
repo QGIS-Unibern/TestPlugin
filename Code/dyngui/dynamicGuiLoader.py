b9451660c9b03da1434f4530df579920f216ca98
Orlando Signer <orlando.signer@students.unibe.ch>
2014-04-27 13:14:22 +0200

    fetching data from polygon

--- Code/dyngui/dynamicGuiLoader.py
+++ Code/dyngui/dynamicGuiLoader.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- dynguiDialog
+ DynamicGuiLoader
                                  A QGIS plugin
  User-defined gui to represent data
                              -------------------
@@ -24,14 +24,22 @@ from PyQt4 import QtCore, QtGui
 from PyQt4.QtGui import QDialog
 from ui_dyngui import Ui_dyngui
 from PyQt4 import QtGui, uic
+import sys, os
 import os.path
+here = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.normpath(os.path.join(here, '../libs/reportlab-3.0/src')))
+sys.path.insert(0, os.path.normpath(os.path.join(here, '../libs/pyspatialite-2.6.1')))
+
+from pyspatialite import dbapi2 as db
 
 
 class DynamicGuiLoader(QDialog):
+
     def __init__(self, guiName, id):
         super(QDialog, self).__init__()
-        gui = "%s/plugin/%s.ui" % (os.path.dirname(__file__), guiName)
         self.id = id
+        self.guiName = guiName
+        gui = "%s/plugin/%s.ui" % (os.path.dirname(__file__), guiName)
         self.ui = uic.loadUi(gui, self)
         # Connect the buttons.
         self.connect(self.ui.buttonCancel_1, QtCore.SIGNAL("clicked()"), self.cancel)
@@ -39,14 +47,47 @@ class DynamicGuiLoader(QDialog):
         self.connect(self.ui.buttonSave_1, QtCore.SIGNAL("clicked()"), self.save)
         self.connect(self.ui.buttonSave_2, QtCore.SIGNAL("clicked()"), self.save)
 
-        self.exec_()
         self.loadData()
+        self.exec_()
         
     def loadData(self):
+        # TODO hardcoded path
+        conn = db.connect("/home/orlandopse/newProject.sqlite")
+        cur = conn.cursor()
+        constData = self.getConstData(cur)
+        varData = self.getVarData(cur)
+        
         print(self.id)
+        print(constData)
+        print(varData)
         
     def save(self):
         print("save")
         
     def cancel(self):
-        print("cancel")
+        self.close()
+        
+    def getConstData(self, cursor):
+        sql = "SELECT * FROM %s WHERE id = ?;" % self.guiName
+        cursor.execute(sql, (self.id))
+        data = cursor.fetchall()
+        map = self.matchDataWithColumnNames(cursor, data[0], self.guiName)
+        return map
+
+    def getVarData(self, cursor):
+        sql = "SELECT * FROM %s WHERE parent_id = ?;" % (self.guiName + "_var")
+        cursor.execute(sql, (self.id))
+        data = cursor.fetchall()
+        map = self.matchDataWithColumnNames(cursor, data, self.guiName + "_var")
+        return map
+    
+    def matchDataWithColumnNames(self, cursor, data, tableName):
+        if not data:
+            return {}
+        sql = "PRAGMA table_info(%s);" % tableName 
+        cursor.execute(sql)
+        names = cursor.fetchall()
+        map = {}
+        for name in names:
+            map[name[1]] =  data[name[0]]
+        return map
--- Code/dyngui/info_point_tool.py
+++ Code/dyngui/info_point_tool.py
@@ -5,7 +5,26 @@ from qgis.core import QgsRectangle
 from PyQt4 import QtCore, QtGui, uic
 from dynamicGuiLoader import DynamicGuiLoader
 
+"""
+/***************************************************************************
+ InfoPointTool
+                                 A QGIS plugin
+ A QgsMapTool that identifies the user selected feature and opens the right dyamic gui.
+                             -------------------
+        begin                : 2014-03-24
+        copyright            : (C) 2014 by QgisUnibe - PSE14
+        email                : qgis.unibern@gmail.com
+ ***************************************************************************/
 
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
 class InfoPointTool(QgsMapTool):   
     def __init__(self, canvas):
         QgsMapTool.__init__(self, canvas)
